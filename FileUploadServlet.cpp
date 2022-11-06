8e6363efa0b9d7e20246c3f143537da52a3b18c1
Ryan Odribege <rodribege@my.bcit.ca>
2022-11-05 17:58:41 -0700

    Update doPost of FileUploadServlet.cpp. Outline plan of attack for implementing the server-side submit functionality

--- FileUploadServlet.cpp
+++ FileUploadServlet.cpp
@@ -102,5 +102,7 @@ void FileUploadServlet::doGet(int sock, char request, char response) {
 }
 
 void FileUploadServlet::doPost(int sock, char request, char response) {
-
+    // Upon submitting form, POST method is invoked.
+    // Submitting the form should save the file (and all other form data i.e caption and date) in the server's own
+    // internal file system. The additional date will be saved in the title of the file
 }

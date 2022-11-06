18b9407d61a91c488f08ffbaa5d699c95fb159f8
Ryan Odribege <rodribege@my.bcit.ca>
2022-11-05 23:36:14 -0700

    Clarify use of inet_aton function

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -22,6 +22,8 @@ ServerSocket::ServerSocket(int port)
     server.sin_port = htons(8888);
     //  Uncomment the call to inet_aton when we want to connect to a specific IP address (instead of localhost).
     //  Make sure to put in the right IP address - 192.168.1.128 is just a placeholder
+
+    // inet_aton converts the IP address (in dot form) into binary (in network byte order) and stores it in the 2nd param.
     //  inet_aton("192.168.1.128", &server.sin_addr);
 
 

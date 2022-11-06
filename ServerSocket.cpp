4315ab5c5c3a489676dd1f7df2496959051558a6
Ryan Odribege <rodribege@my.bcit.ca>
2022-11-05 19:15:12 -0700

    Include inet_aton function to bind socket to specific IP address (instead of any/localhost)

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -20,6 +20,10 @@ ServerSocket::ServerSocket(int port)
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(8888);
+    //  Uncomment the call to inet_aton when we want to connect to a specific IP address (instead of localhost).
+    //  Make sure to put in the right IP address - 192.168.1.128 is just a placeholder
+    //  inet_aton("192.168.1.128", &server.sin_addr);
+
 
     int option;
     getsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &option, reinterpret_cast<socklen_t *>(sizeof(option)));
